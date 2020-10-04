1aa70138b9c4c92c059d6b7aa1ae835628d7a7b1
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-10-04 22:05:55 +0200

    merge questsions

---
+++ data/merge.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+let basePath = './dist/';
+basicQ = JSON.parse(fs.readFileSync(basePath + 'basicQuestions.json'));
+advancedQ = JSON.parse(fs.readFileSync(basePath + 'advancedQuestions.json'));
+
+allQuestions = [
+  ...basicQ.map((q) => {
+    return {
+      ...q,
+      type: 'basic',
+    };
+  }),
+  advancedQ.map((q) => {
+    return {
+      ...q,
+      type: 'advanced',
+    };
+  }),
+];
+
+fs.writeFileSync(basePath + 'questions.json', JSON.stringify(allQuestions));
