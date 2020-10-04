05493ff0c5db978dd6645e5aeb6c897e513447d3
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-10-05 00:40:19 +0200

    add pwa

--- popquiz/src/app/app.module.ts
+++ popquiz/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { QuestionsState } from 'src/app/state/questions.state';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -30,6 +31,7 @@ import { HttpClientModule } from '@angular/common/http';
       disabled: environment.production,
     }),
     NgxsStoragePluginModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
